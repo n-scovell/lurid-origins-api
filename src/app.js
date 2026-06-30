48dfd6ce2ce3cdd0db065e5cbc62822ee0598e00
n-scovell <N8scovell@gmail.com>
2026-06-30 18:18:49 -0400

    update
    Updated app.js file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { setCorsHeaders } from "./_cors.js";
+import { corsMiddleware } from "./_cors.js";
 import movieRoutes from "./routes/movieRoutes.js";
+
 const app = express();
-// Middleware
-app.use(setCorsHeaders);
+
+app.use(corsMiddleware);
 app.use(express.json());
-// Health check
+
 app.get("/", (req, res) => {
   res.json({
     status: "ok",
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
     version: "1.0.0",
   });
 });
-// Routes
+
 app.use("/api/movies", movieRoutes);
+
 export default app;
