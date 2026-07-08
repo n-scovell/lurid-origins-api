6832d96b3a6500d5821fdfa503578975fc380abe
n-scovell <N8scovell@gmail.com>
2026-07-08 10:56:05 -0400

    update
    Added new

--- src/_cors.ts
+++ src/_cors.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 export const corsMiddleware = cors({
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
     "https://luridorigins.vercel.app",
   ],
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
